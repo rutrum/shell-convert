a448453f23a812465c3fee1f4f5725d954439d09
Troy Wiegand <troy@troywiegand.com>
2023-09-09 21:09:31 -0400

    Create Filetypes page

---
+++ src/models/filetype.ts
@@ -0,0 +1,5 @@
+export type Filetype = {
+  name: String,
+  description: String,
+  exts: Array<String>
+}
---
+++ src/routes/filetypes/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+
+import type { Filetype } from '../../models/filetype';
+
+export let data: {filetypes:Filetype[]};
+
+</script>
+
+<h1>Filetypes</h1>
+
+<!-- <div>{JSON.stringify(data)}</div> -->
+
+<ul>
+{#each data.filetypes as {exts, name, description}}
+  <li><h3>{name}</h3>
+    <p>{description}</p>
+    <h4>Extensions</h4>
+    <ul>
+      {#each exts as ext}
+        <li>{ext}</li>
+      {/each}
+    </ul>
+  </li>
+  {:else}
+  <h3>Failure to load File Types</h3>
+{/each}
+</ul>
